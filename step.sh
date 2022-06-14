d79b2d7c01ac53511699c2030f05d7466011649b
Denis Ryzhkov <denis@getfront.com>
2022-06-14 16:49:38 +0300

    Find issues in commits mesage with specified status

--- step.sh
+++ step.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
+CLOSED_TASKS=$(git --no-pager log --pretty='format:%b' -n $commits_count | grep -oE "($project_key-[0-9]+)" | tr ' ' ',')
+CLOSED_TASKS=$(echo $CLOSED_TASKS | tr ' ' ',')
 
+if [ -z "$CLOSED_TASKS" ]; then
+    echo "No tasks to transition found in git log"
+    exit 0
+fi
 
+echo $CLOSED_TASKS
 
+query=$(jq -n \
+    --arg jql "project = $project_key AND status = '$from_status' AND key in ($CLOSED_TASKS)" \
+    '{ jql: $jql, startAt: 0, maxResults: 20, fields: [ "id" ], fieldsByKeys: false }'
+);
 
+echo "Query to be executed in Jira: $query"
 
+tasks_to_close=$(curl -s \
+    -H "Content-Type: application/json" \
+    --user "$jira_user_name:$jira_api_token" \
+    --request POST \
+    --data "$query" \
+    "$jira_url/rest/api/2/search" | jq -r '.issues[].key'
+)
 
-set -ex
+tasks_to_close=$(echo $tasks_to_close | tr ' ' '|')
+echo $tasks_to_close
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
-
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
-
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+envman add --key TASKS_TO_CLOSE --value $tasks_to_close
